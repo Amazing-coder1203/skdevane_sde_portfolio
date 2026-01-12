13b7a9d79a8f08c9bd9c248ae62aecc0c837a323
Shivkumar Devane <Amazing-coder1203@users.noreply.github.com>
2026-01-13 00:52:32 +0530

    Update base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/SKDevane_Data_Portfolio/",
+  base: "/skdevane_sde_portfolio/",
   plugins: [react()],
   resolve: {
     dedupe: ['react', 'react-dom', 'three'],
